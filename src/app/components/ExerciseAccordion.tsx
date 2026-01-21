0d020e2a77338b41e80dc67841d4501a68c7553a
dubovds <dubovds87@gmail.com>
2026-01-21 12:12:55 -0800

    Add app folder structure

---
+++ src/app/components/ExerciseAccordion.tsx
@@ -0,0 +1,3 @@
+export default function ExerciseAccordion() {
+  return <div>Exercise accordion</div>;
+}
---
+++ src/app/components/WorkoutSelector.tsx
@@ -0,0 +1,3 @@
+export default function WorkoutSelector() {
+  return <div>Workout selector</div>;
+}
